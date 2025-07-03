22fcda5df433426ee3d855ef496bacbe717b9f9a
HayrapetyanZhirayr <zhsayrapetyan@yandex.ru>
2025-07-03 11:11:47 +0000

    fix: log config after dist init to escape logging on all ranks

--- recipes/full_finetune_distributed.py
+++ recipes/full_finetune_distributed.py
@@ -1102,8 +1102,8 @@ def recipe_main(cfg: DictConfig) -> None:
         - Parameters specified in config (see available configs through ``tune ls``)
         - Overwritten by arguments from the command-line
     """
-    config.log_config(recipe_name="FullFinetuneRecipeDistributed", cfg=cfg)
     recipe = FullFinetuneRecipeDistributed(cfg=cfg)
+    config.log_config(recipe_name="FullFinetuneRecipeDistributed", cfg=cfg)
     recipe.setup(cfg=cfg)
     recipe.train()
     recipe.cleanup()
